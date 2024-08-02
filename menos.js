b8693f85d4dfd428acd9640c6ac7d5b01f40ad45
abner <abnerjose512@gmail.com>
2024-08-02 16:10:40 -0300

    ok

---
+++ menos.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+
+const servidor = express()
+
+servidor.get('/calculadora/menos', (req, resp) => {
+    let n1 = Number(req.query.n1)
+    let n2 = Number(req.query.n2)
+
+    let menos = n1 - n2
+    resp.send({
+        mensagem:menos
+    })
+
+})
+
+servidor.listen(5001, () => console.log('API subiu na porta'))
